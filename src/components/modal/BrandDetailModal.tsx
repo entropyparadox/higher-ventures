5902ead8d04eff0fd4ad718f662b2fc990277535
wonsun1 <115059315+wonsun1@users.noreply.github.com>
2023-05-04 13:04:34 +0900

    height 수정

--- src/components/modal/BrandDetailModal.tsx
+++ src/components/modal/BrandDetailModal.tsx
@@ -28,44 +28,46 @@ export const BrandDetailModal: FC<BrandDetailModalProps> = ({
     <AnimationLayout open={open} onClose={onClose}>
       <div className="relative h-screen w-screen overflow-hidden">
         <div className="flex h-full w-full transform items-center justify-center bg-black/90 text-left">
-          <Icon.X
-            className="absolute right-14 top-14 z-30 cursor-pointer stroke-white md:top-8 md:right-8"
-            onClick={onClose}
-          />
+          <div className="overflow-y-auto pt-10">
+            <Icon.X
+              className="absolute right-12 top-12 z-30 cursor-pointer stroke-white md:top-8 md:right-8"
+              onClick={onClose}
+            />
 
-          <div className="mt-24 flex max-w-screen-xl flex-col space-x-0 space-y-10 px-5 md:mt-10 lg:flex-row lg:items-center lg:space-y-0 lg:space-x-24">
-            <div className="flex h-[250px] w-[250px] items-center justify-center rounded-lg bg-white p-12 md:h-[400px] md:w-[400px] md:p-20">
-              <Image src={modalContents.modalImg} alt="" className="w-full" />
-            </div>
-
-            <div className="space-y-12 md:flex-1">
-              <div className="space-y-4">
-                <p className="text-[14px] font-semibold text-[#7AA5C3] md:text-[16px]">
-                  Corporation
-                </p>
-                <div className="space-y-1">
-                  <p className="text-[32px] font-bold text-white md:text-[48px]">
-                    {modalContents.title}
-                  </p>
-                  <p className="text-[18px] font-medium text-white md:text-[24px]">
-                    {modalContents.subTitle}
-                  </p>
-                </div>
-                <p className="text-[12px] text-[#BEBEBE] md:text-[16px]">
-                  {modalContents.contents}
-                </p>
+            <div className="mt-24 flex max-w-screen-xl flex-col space-x-0 space-y-10 px-5 md:mt-10 lg:flex-row lg:items-center lg:space-y-0 lg:space-x-24">
+              <div className="flex h-[250px] w-[250px] items-center justify-center rounded-lg bg-white p-12 md:h-[400px] md:w-[400px] md:p-20">
+                <Image src={modalContents.modalImg} alt="" className="w-full" />
               </div>
 
-              <div className="space-y-4">
-                <p className="text-[14px] font-semibold text-[#7AA5C3] md:text-[16px]">
-                  Website
-                </p>
-                <p
-                  className="cursor-pointer text-[18px] font-semibold text-white underline md:text-[24px]"
-                  onClick={() => window.open(modalContents.url)}
-                >
-                  {modalContents.website}
-                </p>
+              <div className="space-y-12 md:flex-1">
+                <div className="space-y-4">
+                  <p className="text-[14px] font-semibold text-[#7AA5C3] md:text-[16px]">
+                    Corporation
+                  </p>
+                  <div className="space-y-1">
+                    <p className="text-[32px] font-bold text-white md:text-[48px]">
+                      {modalContents.title}
+                    </p>
+                    <p className="text-[18px] font-medium text-white md:text-[24px]">
+                      {modalContents.subTitle}
+                    </p>
+                  </div>
+                  <p className="text-[12px] text-[#BEBEBE] md:text-[16px]">
+                    {modalContents.contents}
+                  </p>
+                </div>
+
+                <div className="space-y-4">
+                  <p className="text-[14px] font-semibold text-[#7AA5C3] md:text-[16px]">
+                    Website
+                  </p>
+                  <p
+                    className="cursor-pointer text-[18px] font-semibold text-white underline md:text-[24px]"
+                    onClick={() => window.open(modalContents.url)}
+                  >
+                    {modalContents.website}
+                  </p>
+                </div>
               </div>
             </div>
           </div>
